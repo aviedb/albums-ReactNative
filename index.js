93836c09b746aa502530e659ea8ec100f64fd826
Muhammad Avied Bachmid <aviedbachmid.ab@gmail.com>
2018-02-18 16:37:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+// import { AppRegistry } from 'react-native';
+// import App from './App';
+//
+// AppRegistry.registerComponent('albums', () => App);
 
-AppRegistry.registerComponent('albums', () => App);
+import React from 'react'
+import { AppRegistry, Text } from 'react-native'
+
+const App = () => {
+  return (
+    <Text>Hello World!</Text>
+  )
+}
+
+AppRegistry.registerComponent('albums', () => App)
